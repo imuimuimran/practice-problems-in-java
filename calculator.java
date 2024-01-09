6d09c0e78fc02f592f82e4b317c31f9da18552a4
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-09 13:51:26 +0600

    add calculator in java by switching method.

---
+++ calculator.java
@@ -0,0 +1,42 @@
+import java.util.*;  
+public class calculator {  
+    public static void main(String[] args)  {  
+        Scanner sc= new Scanner(System.in);  
+        System.out.print("Enter First Number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter Second Number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter Operator: ");
+        int operator = sc.nextInt();
+        
+        /**
+        * 1 -> +
+        * 2 -> -
+        * 3 -> *
+        * 4 -> /
+        * 5 -> %
+        */
+
+        switch(operator) {
+            case 1 : System.out.println(a+b);
+            break;
+            case 2 : System.out.println(a-b);
+            break;
+            case 3 : System.out.println(a*b);
+            break;
+            case 4 : if(b == 0) {
+            System.out.println("Invalid Division");
+            } else {
+            System.out.println(a/b);
+            }
+            break;
+            case 5 : if(b == 0) {
+            System.out.println("Invalid Division");
+            } else {
+            System.out.println(a%b);
+            }
+            break;
+            default : System.out.println("Invalid Operator");
+        }          
+    }  
+}  
