79433a73f0ff48e2e2308397a7c6d8d3883cfd19
Mohd. Imran Hossain <hossainmohdimran@gmail.com>
2024-01-14 21:13:43 +0600

    add a function where a number can cahek that it is even or not.

---
+++ functionEvenNumberCheck.java
@@ -0,0 +1,21 @@
+import java.util.*;  
+public class functionEvenNumberCheck {  
+    public static int checkEvenNumber(int n) {
+
+        
+        if(n % 2 == 0) {
+            System.out.println(n + " is an even number.");
+        } else {
+            System.out.println(n + " is not an even number.");
+        }
+       
+        return n;
+    }
+    public static void main(String[] args)  {  
+        Scanner scanner = new Scanner(System.in);  
+        System.out.print("Enter a Number: ");
+        int n = scanner.nextInt(); 
+
+        checkEvenNumber(n);
+    }  
+}  
